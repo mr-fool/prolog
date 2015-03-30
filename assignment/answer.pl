f0c29c348b699f7d6c7a049fae37211ea0ea4361
mr-fool <mr-fool@users.noreply.github.com>
2015-03-30 01:04:23 -0600

    Update answer.pl

--- assignment/answer.pl
+++ assignment/answer.pl
@@ -13,10 +13,10 @@ fatherOf(X,Y) :- parentOf(B,Y), parentOf(X,Y), X \= B, female(B).
 grandparentOf(X,Y) :- parentOf(X,Z), parentOf(Z,Y).
 
 grandmotherOf(X,Y) :- grandparentOf(X,Y), female(X).
-%grandmotherOf(X,Y) :- grandparentOf(X,Y), \+male(X), motherOf(W,Y), motherOf(Z,W).
+grandmotherOf(X,Y) :- grandparentOf(X,Y), \+male(X).
 
 grandfatherOf(X,Y) :- grandparentOf(X,Y), male(X).
-grandfatherOf(X,Y) :- grandparentOf(X,Y),\+female(X), fatherOf(W,Y), fatherOf(Z,W).
+grandfatherOf(X,Y) :- grandparentOf(X,Y),\+female(X).
 
 greatgrandparentOf(X,Y) :- parentOf(X,Z), parentOf(Z,A), parentOf(A,Y).
 
@@ -24,8 +24,7 @@ greatgrandmotherOf(X,Y) :- greatgrandparentOf(X,Y), female(X).
 %greatgrandmotherOf(X,Y) :- greatgrandparentOf(X,Y), \+male(X), motherOf(W,Y), motherOf(Z,W), motherOf(P,Z).
 
 greatgrandfatherOf(X,Y) :- greatgrandparentOf(X,Y), male(X).
-%greatgrandfatherOf(X,Y) :- greatgrandparentOf(X,Y), \+female(X), fatherOf(W,Y), fatherOf(Z,W), fatherOf(P,Z).
-
+greatgrandfatherOf(X,Y) :- greatgrandparentOf(X,Y), \+female(X).
 childOf(X,Y) :- parentOf(Y,X).
 
 daughterOf(X,Y) :- parentOf(Y,X), female(X).
