be6cc4b28bb193a7128d8b4a09de9b3f00dfd7e1
mr-fool <mr-fool@users.noreply.github.com>
2015-04-02 15:00:52 -0600

    Update answer.pl

--- assignment/answer.pl
+++ assignment/answer.pl
@@ -12,22 +12,27 @@ grandmotherOf(X,Y) :- grandparentOf(X,Y), female(X).
 grandmotherOf(X,Y) :- grandparentOf(X,Y), isFemale(X).
 
 grandfatherOf(X,Y) :- grandparentOf(X,Y), male(X).
-grandfatherOf(X,Y) :- grandparentOf(X,Y),\+female(X).
+grandfatherOf(X,Y) :- grandparentOf(X,Y), isMale(X).
 
 greatgrandparentOf(X,Y) :- parentOf(X,Z), parentOf(Z,A), parentOf(A,Y).
 
 greatgrandmotherOf(X,Y) :- greatgrandparentOf(X,Y), female(X).
-%greatgrandmotherOf(X,Y) :- greatgrandparentOf(X,Y), \+male(X), motherOf(W,Y), motherOf(Z,W), motherOf(P,Z).
+greatgrandmotherOf(X,Y) :- greatgrandparentOf(X,Y), isFemale(X).
 
 greatgrandfatherOf(X,Y) :- greatgrandparentOf(X,Y), male(X).
-greatgrandfatherOf(X,Y) :- greatgrandparentOf(X,Y), \+female(X).
+greatgrandfatherOf(X,Y) :- greatgrandparentOf(X,Y), isMale(X).
+
 childOf(X,Y) :- parentOf(Y,X).
 
-daughterOf(X,Y) :- parentOf(Y,X), female(X).
+daughterOf(X,Y) :- parentOf(Y,X), isFemale(X).
 
-sonOf(X,Y) :- parentOf(Y,X), male(X).
+sonOf(X,Y) :- parentOf(Y,X), isMale(X).
 
 grandchildOf(X,Y) :- parentOf(Y,Z), parentOf(Z,X).
+
+granddaughterOf(X,Y) :- grandparentOf(Y,X), isFemale(Y).
+
+
 
 isMale(A) :- parentOf(B, Y), parentOf(A, Y), A \= B, female(B).
 isFemale(A) :- parentOf(B, Y), parentOf(A, Y), A \= B, male(B).
