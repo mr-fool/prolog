613c0d073d285a37914281897903eea3484714ea
mr-fool <mr-fool@users.noreply.github.com>
2015-04-05 18:18:16 -0600

    Update answer.pl

--- assignment/answer.pl
+++ assignment/answer.pl
@@ -4,5 +4,8 @@ no
 | ?- ancestorOf(joe,mavis).
 
 true ? a
+| ?- ancestorOf(X,mavis).  
+
+no
 ancestorOf(X,Y) :- X \= Y, parentOf(X, Y).
 ancestorOf(X,Y) :- X \= Y, parentOf(X, Z), ancestorOf(Z,Y).
