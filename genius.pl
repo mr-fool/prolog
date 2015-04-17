94337a1c40127bd1e271b55ce227bb0415c50f2a
mr-fool <mr-fool@users.noreply.github.com>
2015-04-16 19:17:36 -0600

    Update genius.pl

--- genius.pl
+++ genius.pl
@@ -1,11 +1,14 @@
-invented(edision,lightbulb).
+invented(edison,lightbulb).
 invented(colmeraurer,prolog).
 iq(einstein,210).
 iq(edison,160).
 iq(waldorf,90).
-genius(Person) :-
-	iq(Person,Iq),
+genius(Person):-
+	iq(Person,IQ),
 	IQ > 150.
-genius(Person) :- invented(Person,_).
-
-no_beam :- genius(Person), write('I found intelligent life, sir!'), nl.
+genius(Person):-
+	invented(Person,_).
+smart_invention(Invention):- invented(Person,Invention), iq(Person,IQ), IQ >= 160 .
+no_beam:-
+	genius(Person),
+	write('I found intelligent life, sir!'), nl.
